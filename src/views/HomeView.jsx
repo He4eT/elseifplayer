42015eca8fb9d72f8aebb1c59c4c90a62cb51c64
He4eT <He4eTHb1u@gmail.com>
2021-03-02 02:30:22 +0500

    Dirty markup

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -24,88 +24,93 @@ export default function ({ themeEngine }) {
         ifplayer
       </h1>
 
-      <p>
-        Interactive Fiction interpreter
-        that runs right in your browser.
-        <br />
-        Powered by <a
-          target="_blank"
-          href="https://github.com/He4eT/emglken">
-          Emglken
-        </a> and <a
-          target="_blank"
-          href="https://github.com/He4eT/cheap-glkote">
-          cheap-glkote
-        </a>.
-      </p>
-      <p>
-        Source code can be found here: <a
-          target="_blank"
-          href="https://github.com/He4eT/ifplayer">
-          ifplayer
-        </a>.
-      </p>
+      <section>
+        <p>
+          Interactive Fiction interpreter
+          that runs right in your browser.
+          <br/>
+          Source code can be found in this <a
+            target="_blank"
+            href="https://github.com/He4eT/ifplayer">
+            repository
+          </a>.
+        </p>
+      </section>
+
       <hr />
-      <h3>
-        Interface theme
-      </h3>
-      <p>
-        <ThemeSelector {...{
-          themeEngine
-        }} />
-        <br /><br />
-        <small>
-          Themes without borders
-          are better suited for small screens.
-        </small><br />
-        <small>
-          Double-click on the input field during the game
-          to enter full-screen mode.
-        </small>
-      </p>
-      <hr />
-      <h3>
-        Play a game from the list
-      </h3>
-      <p>
-        <Link href='/top100'>IFDB Top 100 games</Link>
-      </p>
-      <hr />
-      <h3>
-        Play the game from a file
-      </h3>
-      <p>
-        <details>
-          <summary>Supported formats</summary>
-          <ul>
-            <li>TADS games (.t3, .gam);</li>
-            <li>Z-machine games (.z3, .z4, .z5, .z8, .blorb);</li>
-            <li>Glulx VM games (.gblorb, .ulx);</li>
-            <li>Hugo games (.hex);</li>
-            <li>Text-only games are supported;</li>
-          </ul>
-        </details>
-      </p>
-      <p className='play'>
-        <label>
-          <div>
-            Local file:
-          </div>
-          <FileSelector {...{
-            setTargetName,
-            setTargetUrl
+
+      <section>
+        <h3>
+          Interface theme
+        </h3>
+        <p>
+          <ThemeSelector {...{
+            themeEngine
           }} />
-        </label>
-        <label>
-          <div>
-            Direct link:
-          </div>
-          <URLSelector {...{
-            setTargetName,
-            setTargetUrl
-          }} />
-        </label>
-      </p>
+          <br /><br />
+          <small>
+            Themes without borders
+            are better suited for small screens.
+          </small>
+          <br />
+          <small>
+            Double-click on the input field during the game
+            to enter full-screen mode.
+          </small>
+        </p>
+      </section>
+
+      <hr />
+
+      <section>
+        <h3>
+          Play a game from the list
+        </h3>
+
+        <p>
+          <Link href='/top100'>
+            IFDB Top 100 games
+          </Link>
+        </p>
+      </section>
+
+      <hr />
+
+      <section>
+        <h3>
+          Play the game from a file
+        </h3>
+        <p>
+          <details>
+            <summary>Supported formats</summary>
+            <ul>
+              <li>TADS games (.t3, .gam);</li>
+              <li>Z-machine games (.z3, .z4, .z5, .z8, .blorb);</li>
+              <li>Glulx VM games (.gblorb, .ulx);</li>
+              <li>Hugo games (.hex);</li>
+              <li>Text-only games are supported;</li>
+            </ul>
+          </details>
+        </p>
+        <p className='play'>
+          <label>
+            Local file: <br />
+            <FileSelector {...{
+              setTargetName,
+              setTargetUrl
+            }} />
+          </label>
+          <br />
+          <label>
+            Direct link: <br />
+            <URLSelector {...{
+              setTargetName,
+              setTargetUrl
+            }} />
+          </label>
+        </p>
+      </section>
+
       { targetUrl
         ? playButton(targetName, targetUrl, themeEngine.currentTheme)
         : null }
