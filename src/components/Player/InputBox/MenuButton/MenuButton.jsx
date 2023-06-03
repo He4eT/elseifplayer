0ad6f7b3357bae9fe1f5f7392c3a03b3912601a0
He4eT <He4eT@oddsquat.org>
2023-06-03 23:39:45 +0300

    MenuButton: add svg icon

--- src/components/Player/InputBox/MenuButton/MenuButton.jsx
+++ src/components/Player/InputBox/MenuButton/MenuButton.jsx
@@ -7,9 +7,15 @@ export default MenuButton = ({ onClick }) => {
     <button
       aria-label='Menu'
       className={s.menuButton}
-      onClick={() => setMenuOpen(true)}
+      onClick={onClick}
     >
-      =
+      <svg
+        class={s.menuIcon}
+        viewBox='0 0 24 24'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <path d='M10 10h4v4h-4zm0-6h4v4h-4zm0 12h4v4h-4z'/>
+      </svg>
     </button>
   )
 }
