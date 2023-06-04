e0a90005e62a7aef98bcfee77b25f000cc2db4e3
He4eT <He4eT@oddsquat.org>
2023-06-05 00:47:23 +0300

    routing: ignore repeats

--- src/routing.js
+++ src/routing.js
@@ -1,33 +1,39 @@
 import {
-  useState, useEffect, useCallback,
+  useCallback, useEffect, useState,
 } from 'preact/hooks'
 
-export const useHashLocation = () => {
-  const currentLoc = () =>
-    window.location.hash.replace('#', '') || '/'
+const windowLocation = () =>
+  window.location.hash.replace('#', '') || '/'
 
-  const [loc, setLoc] = useState(currentLoc())
-
-  useEffect(() => {
-    const handler = () => {
-      setLoc(currentLoc())
-      window.scrollTo(0, 0)
-    }
-
-    handler()
-    window.addEventListener('hashchange', handler)
-    return () => window.removeEventListener('hashchange', handler)
-  }, [])
-
-  const navigate = useCallback((to) =>
-    (window.location.hash = to.replace('#/', '')), [])
-  return [loc, navigate]
-}
-
-export const buildPlayLinkHref = ({ url }) =>
+export const buildPlayLinkHref = ({url}) =>
   `/#/play/${encodeURIComponent(url)}`
 
 export const extractView = (location) => {
   const currentView = location.split('/').filter(Boolean)[0]
   return currentView || ''
+}
+
+export const useHashLocation = () => {
+  const [currentLocation, setCurrentLocation] =
+    useState(windowLocation())
+
+  useEffect(() => {
+    const onHashChange = () => {
+      let newLocation = windowLocation()
+      if (newLocation !== currentLocation) {
+        setCurrentLocation(newLocation)
+        window.scrollTo(0, 0)
+      }
+    }
+
+    onHashChange()
+    window.addEventListener('hashchange', onHashChange)
+    return () => window.removeEventListener('hashchange', onHashChange)
+  }, [currentLocation, setCurrentLocation])
+
+  const navigate = useCallback((to) => {
+    window.location.hash = to.replace('#/', '')
+  }, [])
+
+  return [currentLocation, navigate]
 }
